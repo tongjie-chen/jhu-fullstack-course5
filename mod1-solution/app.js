48d0832d3f8abe7c0e9c3ea5505e6f0c68dab755
tongjie <chenphy10@gmail.com>
2023-07-01 21:56:36 -0400

    Conform on empty and controller naming

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -2,13 +2,17 @@
     "use strict";
 
     angular.module("LunchCheck", [])
-
-        .controller("LunchCheckCtrl", LunchCheckCtrl);
-    LunchCheckCtrl.$inject = ['$scope'];
-    function LunchCheckCtrl($scope) {
+        .controller("LunchCheckController", LunchCheckController);
+    
+    LunchCheckController.$inject = ['$scope'];
+    
+    function LunchCheckController($scope) {
         $scope.message = "Please enter data first";
+        $scope.selection = "";
         $scope.checkFood = function (){
-            if ($scope.selection.split(",").length > 3)
+            if ($scope.selection.length == 0)
+                $scope.message = "Empty!";
+            else if ($scope.selection.split(",").length > 3)
                 $scope.message = "Too much!";
             else
                 $scope.message = "Enjoy!";
