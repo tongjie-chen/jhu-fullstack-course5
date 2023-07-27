2708f2a9df3dbd2ef067f3d90ff548c645abf076
tongjie <chenphy10@gmail.com>
2023-07-26 23:26:36 -0400

    Assignment3: cleaned comments and deal empty search

--- mod3-solution/app.js
+++ mod3-solution/app.js
@@ -1,28 +1,23 @@
 angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
-.directive('listItemDescription', ListItemDescription)
+    .directive('listItemDescription', ListItemDescription)
     .directive('foundItems', foundItems);
 
 NarrowItDownController.inject=['$scope', '$http', 'MenuSearchService']
 function NarrowItDownController($scope, $http, MenuSearchService) {
-    // var getMatchedMenuItems = MenuSearchService.getMatchedMenuItems;
-    // var found = MenuSearchService.getMatchedMenuItems("chicken");
-    // console.log(found);
     $scope.getMatchedMenuItems = MenuSearchService.getMatchedMenuItems;
     $scope.matchFun = function(result){
-        $scope.found = Object.values(result.data).map(function(it){return it.menu_items}).flat(1).filter(function(it){return it.description.includes($scope.searchTerm.toLowerCase())});
+        if ($scope.searchTerm) {
+            $scope.found = Object.values(result.data).map(function(it){return it.menu_items}).flat(1).filter(function(it){return it.description.includes($scope.searchTerm.toLowerCase())});
+        }
+        else
+            $scope.found = [];
         console.log($scope.found);
     };
-    $scope.removeItem = function(index) {
+    $scope.onRemove = function(index) {
         $scope.found.splice(index, 1);
     }
-    // var found_promise = $scope.getMatchedMenuItems('broth');
-    // // $scope.searchTerm = "beef"
-    // found_promise.then(function(result){
-    //     var foundItems = Object.values(result.data).map(function(it){return it.menu_items}).flat(1).filter(function(it){return it.description.includes($scope.searchTerm.toLowerCase())});
-    //     console.log(foundItems);
-    // })
 }
 
 MenuSearchService.inject=['$http']
@@ -31,31 +26,28 @@ function MenuSearchService($http) {
     
     this.getMatchedMenuItems = function(searchTerm) {
         console.log(searchTerm);
-        // var foundItems;
         return $http({
             url: "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json"
         })
-            // .then(function(result){
-            // this.foundItems = Object.values(result.data).map(function(it){return it.menu_items}).flat(1).filter(function(it){return it.description.includes(searchTerm.toLowerCase())});
-            // return foundItems;
-        // });
-        return this.foundItems;
     }
-    // var test = this.getMatchedMenuItems('chicken');
-    // console.log(test);
 }
 
 function foundItems() {
     var ddo = {
+        // scope: {
+        //     found: "<"
+        //     // onRemove: 
+        // },       
+        
         templateUrl:"foundItems.template.html"
     };
     return ddo;
 }
 
 function ListItemDescription() {
-  var ddo = {
-    template: '{{ item.quantity }} of {{ item.name }}'
-  };
+    var ddo = {
+        template: '{{ item.name }}'
+    };
 
-  return ddo;
+    return ddo;
 }
