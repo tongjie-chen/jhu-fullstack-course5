571f3318a0ee13e3b56e97349595b745de3589dc
tongjie <chenphy10@gmail.com>
2023-08-01 21:02:43 -0400

    Assignment4: move template and fix sref with parameter

--- mod4-solution/categories.component.js
+++ mod4-solution/categories.component.js
@@ -1,7 +1,7 @@
 angular.module('MenuApp')
     .component('categories', {
         // 'controller': "CategoryCtrl",
-        'template': '<li ui-sref-active="active"><a ui-sref="/items/{{$ctrl.cat.short_name}}">{{$ctrl.cat.name}}</a></li>',
+        'templateUrl': 'categories.template.html',
         // 'template': '<li ui-sref-active="active"><a href="/items/A">{{$ctrl.cat.name}}</a></li>',        
         'bindings': {
             cat: '<'
